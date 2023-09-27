669e4448b596b1b4a068fc11885f447d3eb8c17f
Kapitanov Andrey <kapitanov@greensight.ru>
2023-09-27 19:22:50 +0300

    ENSITECH-1

--- src/Commands/GenerateClient.php
+++ src/Commands/GenerateClient.php
@@ -12,29 +12,30 @@ abstract class GenerateClient extends Command
 
     /** Generator name, one of valid openapi generators names */
     protected string $generator;
-
-    protected string $apidocDir;
-
+    protected string $apiDocDir;
+    protected bool $asyncApiEnabled;
+    protected string $asyncApiOutputDir;
+    protected string $asyncApiDocDir;
+    protected string $asyncApiTemplateDir;
     protected string $outputDir;
-
     protected string $gitUser;
-
     protected string $gitRepo;
-
     protected string $gitHost;
-
     protected array $params;
-
     protected string $templateDir;
-
     protected array $filesToIgnoreDuringCleanup;
 
     public function __construct()
     {
         parent::__construct();
 
-        $this->apidocDir = config('openapi-client-generator.apidoc_dir');
+        $this->apiDocDir = config('openapi-client-generator.apidoc_dir');
         $this->outputDir = config('openapi-client-generator.output_dir_template') . "-$this->client";
+
+        $this->asyncApiEnabled = config('openapi-client-generator.async_api.enabled', false);
+        $this->asyncApiTemplateDir = config('openapi-client-generator.async_api.template_dir', $this->getDefaultAsyncApiTemplateDir());
+        $this->asyncApiDocDir = config('openapi-client-generator.async_api.doc_dir', $this->getDefaultAsyncApiDocDir());
+        $this->asyncApiOutputDir = config('openapi-client-generator.async_api.output_dir', $this->getDefaultAsyncApiOutputDir());
 
         $this->gitUser = config('openapi-client-generator.git_user', '');
         $this->gitRepo = config('openapi-client-generator.git_repo_template', '') . "-$this->client";
@@ -51,9 +52,13 @@ abstract class GenerateClient extends Command
     public function handle(): int
     {
         $this->recursiveClearDirectory($this->outputDir);
-        if (self::FAILURE === $this->generateClientPackage()) {
+        if ($this->generateOpenApiClient() === self::FAILURE) {
             return self::FAILURE;
         }
+        if ($this->generateAsyncApiClient() === self::FAILURE) {
+            return self::FAILURE;
+        }
+
         $this->patchClientPackage();
         $this->copyLicenseToClientPackage();
 
@@ -62,15 +67,39 @@ abstract class GenerateClient extends Command
 
     abstract protected function patchClientPackage(): void;
 
-    private function generateClientPackage(): int
+    private function generateOpenApiClient(): int
     {
         $bin = 'npx --yes @openapitools/openapi-generator-cli';
-        $i = escapeshellarg($this->apidocDir . DIRECTORY_SEPARATOR . "index.yaml");
+        $i = escapeshellarg($this->apiDocDir . DIRECTORY_SEPARATOR . "index.yaml");
         $g = escapeshellarg($this->generator);
         $o = escapeshellarg($this->outputDir);
         $command = "$bin generate -i $i -g $g -o $o " . $this->getGeneratorArguments();
 
         $this->info("Generating $this->client client by command: $command");
+
+        $output = null;
+        $resultCode = null;
+        exec($command, $output, $resultCode);
+
+        return $resultCode;
+    }
+
+    private function generateAsyncApiClient(): int
+    {
+        if (!$this->asyncApiEnabled) {
+            $this->info("Generating AsyncApi skipped");
+
+            return self::SUCCESS;
+        }
+
+        $path = implode(DIRECTORY_SEPARATOR, [$this->asyncApiTemplateDir, "generate.js"]);
+
+        // $i = escapeshellarg($inputPath);
+        $o = escapeshellarg($this->asyncApiOutputDir);
+        $f = escapeshellarg($this->asyncApiDocDir);
+        $command = "node $path -o $o -f $f";
+
+        $this->info("Generating AsyncApi $this->client client by command: $command");
 
         $output = null;
         $resultCode = null;
@@ -180,5 +209,20 @@ abstract class GenerateClient extends Command
         if ($level > 0) {
             rmdir($dir);
         }
+    }
+
+    private function getDefaultAsyncApiTemplateDir(): string
+    {
+        return implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "AsyncAPI"]);
+    }
+
+    private function getDefaultAsyncApiOutputDir(): string
+    {
+        return $this->outputDir . DIRECTORY_SEPARATOR . "/async";
+    }
+
+    private function getDefaultAsyncApiDocDir(): string
+    {
+        return '/var/www/public/async-docs';
     }
 }
