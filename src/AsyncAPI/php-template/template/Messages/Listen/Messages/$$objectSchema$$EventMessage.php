b62d197f92b48d26fe318eca97debdd374eb1f39
Kapitanov Andrey <kapitanov@greensight.ru>
2023-09-28 11:16:46 +0300

    ENSITECH-1

--- src/AsyncAPI/php-template/template/Messages/Listen/Messages/$$objectSchema$$EventMessage.php
+++ src/AsyncAPI/php-template/template/Messages/Listen/Messages/$$objectSchema$$EventMessage.php
@@ -3,6 +3,7 @@
 namespace {{ params.packageName | safe }}\Messages\Listen\Messages;
 
 use {{ params.packageName | safe }}\Messages\Listen\Payloads\{{schemaName | camelCase | upperFirst}}Payload;
+use RdKafka\Message;
 
 /**
  * @property {{schemaName | camelCase | upperFirst}}Payload $attributes
