d19ea0bc360f8289b1ad2a9ba89f186047b89308
Kapitanov Andrey <kapitanov@greensight.ru>
2023-11-08 17:17:17 +0300

    ENSITECH-1

--- src/AsyncAPI/php-template/template/Messages/Listen/$$message$$.php
+++ src/AsyncAPI/php-template/template/Messages/Listen/$$message$$.php
@@ -7,13 +7,15 @@ namespace {{ params.packageName | safe }}\Messages\Listen;
 use {{ params.packageName | safe }}\Messages\Listen\Payloads\{{payload.uid() | camelCase | upperFirst}};
 use RdKafka\Message;
 
-/**
-* @method {{payload.uid() | camelCase | upperFirst}} getPayload()
-*/
 class {{messageName | camelCase | upperFirst}} extends MessageContract
 {
     public function __construct(array $payload)
     {
         $this->setPayload(new {{payload.uid() | camelCase | upperFirst}}($payload));
+    }
+
+    public function getPayload(): {{payload.uid() | camelCase | upperFirst}}
+    {
+        return $this->payload;
     }
 }
