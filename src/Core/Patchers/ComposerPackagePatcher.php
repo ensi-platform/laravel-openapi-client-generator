a3390c33c3c8d94e81b88647075a0bf78e0915f8
valeriya.lukinykh <valerialukinykh@gmail.com>
2025-07-29 12:49:26 +0700

    ENSITECH-273 php8.4 support [2]

--- src/Core/Patchers/ComposerPackagePatcher.php
+++ src/Core/Patchers/ComposerPackagePatcher.php
@@ -42,7 +42,7 @@ class ComposerPackagePatcher extends PackageManifestPatcher
     {
         $manifest['require']['php'] = '^7.1 || ^8.0';
         $manifest['require']['guzzlehttp/guzzle'] = '^6.2 || ^7.0';
-        $manifest['require']['guzzlehttp/psr7'] = '^1.6.1 || ^2.7.0';
+        $manifest['require']['guzzlehttp/psr7'] = '^1.6.1 || ^2.6.3';
 
         return $manifest;
     }
